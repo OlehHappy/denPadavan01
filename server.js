e85f998eccce5d1f7e0608067efe4cafa5df30b4
OlehHappy <olegtranslator@gmail.com>
2016-05-16 13:23:53 +0200

    sever with angular app working

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-var express = require('express');
+var express = require('express'),
+	app = express()
+	path = require('path');
 
-// bla bla
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
+	console.log('Server started on PORT: ' + PORT);
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, './app/index.html'));
+});
